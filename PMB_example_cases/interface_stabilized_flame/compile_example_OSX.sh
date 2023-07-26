2371f9ec1b1af1ac847228c354fba5b70e5a4aaf
ThorstenZirwes <g3bk47@users.noreply.github.com>
2023-07-26 19:55:50 +0200

    Create compile_example_OSX.sh
    Signed-off-by: ThorstenZirwes <g3bk47@users.noreply.github.com>

---
+++ PMB_example_cases/interface_stabilized_flame/compile_example_OSX.sh
@@ -0,0 +1,1 @@
+g++ example_interface_stabilized.cpp -std=c++11 -O2 -pthread -DNDEBUG -I${HOME}/Programs/Cantera/Cantera_PMB_install/include/ -lcantera  -framework Accelerate  -L${HOME}/Programs/Cantera/Cantera_PMB_install/lib/ -o example && ./example && python plot.py
