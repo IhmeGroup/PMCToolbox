242aea7f900ae53687419ce9de91ec13419c0a55
ThorstenZirwes <thorsten@zirwes.org>
2023-09-08 18:40:53 +0200

    Update plot.py
    Signed-off-by: ThorstenZirwes <thorsten@zirwes.org>

--- PMB_example_cases/FPF_PMB/plot.py
+++ PMB_example_cases/FPF_PMB/plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-new = np.genfromtxt("result_phi_1.00_fuel_NH3:0.85,O2:0.15_mdot_0.40.txt",unpack=True)
+new = np.genfromtxt("result_phi_1.30_fuel_NH3:0.7,H2:0.3_mdot_0.40.txt",unpack=True)
 
 plt.plot(new[0],new[1],'-r',label="Tg")
 plt.plot(new[0],new[2],'--k',label="Ts")
